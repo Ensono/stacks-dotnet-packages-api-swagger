7897e72d86c3d5c38752b264f01bc9f7162ab446
Chris Butler <chris.butler@amido.com>
2020-12-02 10:14:09 +0000

    adding base filter class

---
+++ src/Amido.Stacks.API.Swagger/Filters/BasePathFilter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Amido.Stacks.API.Swagger.Filters
+{
+    public class BasePathFilter : IDocumentFilter
+    {
+        public BasePathFilter(string basePath)
+        {
+            BasePath = basePath;
+        }
+
+        public string BasePath { get; }
+
+        public void Apply(OpenApiDocument openApiDocument, DocumentFilterContext context)
+        {
+            if (string.IsNullOrWhiteSpace(BasePath))
+            {
+                return;
+            }
+
+            openApiDocument.Servers = new List<OpenApiServer>
+            {
+                new OpenApiServer {Url = BasePath}
+            };
+        }
+    }
+}
